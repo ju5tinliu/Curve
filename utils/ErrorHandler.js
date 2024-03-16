d02add2f06c7fcd14441f3f80639c966e8240c7f
Arav Kumar <replytoak10@gmail.com>
2024-03-16 11:01:44 -0700

    Reverting to working version

--- utils/ErrorHandler.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(err, req, res, next) {
-    console.error(err);
-    const status = err.status || 500;
-    const message = err.message || 'Something went wrong';
-    res.status(status).json({ error: message });
-  }
-  
-  module.exports = errorHandler;
